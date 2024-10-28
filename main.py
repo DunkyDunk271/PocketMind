8bb01f6cfe3884715ec95fb6546891cc25403bde
DunkyDunk271 <nqd2701@gmail.com>
2024-10-28 23:10:50 +0700

    Add API_Key reading and test API call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from infi.systray import SysTrayIcon
 from openai import OpenAI
-client = OpenAI()
-
+import openai
+import os
 
 def say_hello(systray):
     print("Hello, World!")
@@ -9,16 +9,27 @@ menu_options = (("Say Hello", None, say_hello),)
 systray = SysTrayIcon("icon.ico", "Example tray icon", menu_options)
 systray.start()
 
-def answer():
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {
-                "role": "user",
-                "content": "Write a haiku about recursion in programming."
-            }
-        ]
-    )
+#Promp for API key input if none is found
+api_file = "api_key.txt"
 
-    print(completion.choices[0].message)
+if not os.path.exists(api_file):
+    api_key = input("Please enter your OpenAI API key: ")
+    api_file = open("api_key.txt", "w")
+    api_file.write(api_key)
+    api_file.close()
+else:
+    api_file = open("api_key.txt", "r")
+    api_key = api_file.read()
+    api_file.close()
+
+client = OpenAI(api_key=api_key)
+
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
